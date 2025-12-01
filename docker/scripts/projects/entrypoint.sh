48ced6bf3dbaed2e60944a3b1a8ffc0e161487c7
Stéphane Manciot <stephane.manciot@gmail.com>
2025-12-01 18:02:15 +0100

    get rid of starlake-nas with default docker-compose

--- docker/scripts/projects/entrypoint.sh
+++ docker/scripts/projects/entrypoint.sh
@@ -3,11 +3,6 @@
 set -e
 
 
-# wait for filestore to be ready
-mkdir -p $FILESTORE_MNT_DIR
-mount -v -o nolock $FILESTORE_IP_ADDRESS:/$FILESTORE_SHARE_NAME $FILESTORE_MNT_DIR
-mount
-
 export PGPASSWORD="${POSTGRES_PASSWORD}"
 member_id=$(psql -h "${POSTGRES_HOST}" -U "${POSTGRES_USER}" -d "${POSTGRES_DB}" -w -t -A -c "SELECT id FROM public.slk_member WHERE email = 'admin@localhost.local'")
 echo "Member ID: $member_id"
@@ -28,7 +23,7 @@ if [[ $member_id =~ ^[0-9]+$ ]]; then
                 project_id=$(basename "$id")
                 if [[ $project_id =~ ^[0-9]+$ ]]; then
                     project_name=$(basename "$zip" | cut -d. -f1)
-                    if [ ! -d $FILESTORE_MNT_DIR/$member_id/$project_id ]; then
+                    if [ ! -d /projects/$member_id/$project_id ]; then
                         echo "Project $project_name will be created with id $project_id and UUID $project_uuid"
                         psql -v ON_ERROR_STOP=1 -h "${POSTGRES_HOST}" -U "${POSTGRES_USER}" -d "${POSTGRES_DB}" -w <<-EOSQL
 INSERT INTO public.slk_project (id, code, "name", description, repository, active, deleted, created, updated, master, owner, owner_email, access, pat, airflow_role)
@@ -39,16 +34,16 @@ OVERRIDING SYSTEM VALUE
 VALUES($project_id, $project_id, '[{"envName":"__sl_ignore__"}]', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 EOSQL
                         echo "Move project $member_id/$project_id to target folder"
-                        mkdir -p $FILESTORE_MNT_DIR/$member_id/$project_id
-                        cp -r $id/* $FILESTORE_MNT_DIR/$member_id/$project_id
+                        mkdir -p /projects/$member_id/$project_id
+                        cp -r $id/* /projects/$member_id/$project_id
                         rm -rf /projects/$project_uuid
                         echo "Init git repository"
-                        git -C "$FILESTORE_MNT_DIR/$member_id/$project_id" init
-                        git -C "$FILESTORE_MNT_DIR/$member_id/$project_id" config user.email "admin@localhost.local"
-                        git -C "$FILESTORE_MNT_DIR/$member_id/$project_id" config user.name "admin@localhost.local"
-                        git -C "$FILESTORE_MNT_DIR/$member_id/$project_id" config pull.rebase false
+                        git -C "/projects/$member_id/$project_id" init
+                        git -C "/projects/$member_id/$project_id" config user.email "admin@localhost.local"
+                        git -C "/projects/$member_id/$project_id" config user.name "admin@localhost.local"
+                        git -C "/projects/$member_id/$project_id" config pull.rebase false
                     else
-                        echo "Project $project_id is already present in $FILESTORE_MNT_DIR/$member_id/"
+                        echo "Project $project_id is already present in /projects/$member_id/"
                         rm -rf /projects/$project_uuid
                     fi
                 else
